a4062f15680adc5d5e793d33f3014789c4270cba
Doug Johnson <finson@whidbey.com>
2015-11-08 15:44:50 -0800

    Start to formulate device driver API.

---
+++ libraries/FirmataFeature/DeviceDriver.h
@@ -0,0 +1,19 @@
+#ifndef DeviceDriver_h
+#define DeviceDriver_h
+
+#include <FirmataFeature.h>
+
+class DeviceDriver: public FirmataFeature
+{
+  public:
+
+    // int open(char *controllerName);
+    // boolean status(int handle);
+    // boolean control(int handle);
+    // boolean read(int handle);
+    // boolean write(int handle);
+    // boolean close(int handle);
+
+};
+
+#endif
