ae1e9911c9319de39f10f24e4eacb5101196a5af
Doug Johnson <finson@whidbey.com>
2015-12-07 00:16:21 -0800

    Define and use the HelloDriver instance.

--- FirmataCoreMain/SelectedFeatures.h
+++ FirmataCoreMain/SelectedFeatures.h
@@ -27,10 +27,10 @@ FirmataFeature *selectedFeatures[] = {&deviceManager,0};
 // Device Drivers
 
 #include <Hello/HelloDriver.h>
-HelloDriver ddHello("Hello");
+HelloDriver ddHello("Hello",4);
 
 // #include <MCP9808/MCP9808Driver.h>
 // MCP9808Driver ddMCP9808("MCP9808",0x18,1);
 
 DeviceDriver *selectedDevices[] = {&ddHello,0};
-// DeviceDriver *selectedDevices[] = {&ddMCP9808,0};
+// DeviceDriver *selectedDevices[] = {&ddHello,&ddMCP9808,0};
