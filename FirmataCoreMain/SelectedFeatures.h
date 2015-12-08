b919992aafbcb14d64d8d9ab228710d5bbb993dd
Doug Johnson <finson@whidbey.com>
2015-12-07 18:30:27 -0800

    Enable all features for testing.

--- FirmataCoreMain/SelectedFeatures.h
+++ FirmataCoreMain/SelectedFeatures.h
@@ -1,27 +1,27 @@
 
 // Firmata Features
 
-// #include <GPIO/DigitalInputFeature.h>
-// DigitalInputFeature digitalInput;
+#include <GPIO/DigitalInputFeature.h>
+DigitalInputFeature digitalInput;
 
-// #include <GPIO/DigitalOutputFeature.h>
-// DigitalOutputFeature digitalOutput;
+#include <GPIO/DigitalOutputFeature.h>
+DigitalOutputFeature digitalOutput;
 
-// #include <GPIO/AnalogInputFeature.h>
-// AnalogInputFeature analogInput;
+#include <GPIO/AnalogInputFeature.h>
+AnalogInputFeature analogInput;
 
-// #include <GPIO/AnalogOutputFeature.h>
-// AnalogOutputFeature analogOutput;
+#include <GPIO/AnalogOutputFeature.h>
+AnalogOutputFeature analogOutput;
 
 #include <DeviceFeature.h>
 DeviceFeature deviceManager;
 
 // Firmata Features (Communications)
 
- // #include <I2C/I2CFeature.h>
- // I2CFeature pI2C;
+#include <I2C/I2CFeature.h>
+I2CFeature pI2C;
 
-FirmataFeature *selectedFeatures[] = {&deviceManager,0};
+FirmataFeature *selectedFeatures[] = {&digitalInput,&digitalOutput,&analogInput,&analogOutput,&pI2C,&deviceManager,0};
 // FirmataFeature *selectedFeatures[] = {&digitalInput,&digitalOutput,&analogInput,&analogOutput,&pI2C,&deviceManager,0};
 
 // Device Drivers
