f190cf2b25947c2d327b1b8e9ca2879d3fabf32e
Doug Johnson <finson@whidbey.com>
2015-12-16 17:31:43 -0800

    Add error code defines as needed.

--- libraries/FirmataDevice/src/Device/DeviceError.h
+++ libraries/FirmataDevice/src/Device/DeviceError.h
@@ -6,6 +6,10 @@
 #define ENODEV      19  /* No such device */
 #define ENOSYS      38  /* Function not implemented */
 #define EMSGSIZE    90  /* Message too long */
-
+#define EADDRINUSE  98  /* Address already in use */
+#define EISCONN     106 /* Transport endpoint is already connected */
+#define ENOTCONN    107 /* Transport endpoint is not connected */
+#define ECONNREFUSED    111 /* Connection refused */
+#define EREMOTEIO   121 /* Remote I/O error */
 
 #endif
