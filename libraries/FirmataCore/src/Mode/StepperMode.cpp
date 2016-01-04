419bc3e978b610068a349b5d59f908d6eb634316
Doug Johnson <finson@whidbey.com>
2016-01-03 23:11:57 -0800

    Start (restart, actually) stepper mode development.

--- libraries/FirmataCore/src/Mode/StepperMode.cpp
+++ libraries/FirmataCore/src/Mode/StepperMode.cpp
@@ -3,23 +3,17 @@
  * from software on a host computer. It is intended to work with
  * any host computer software package.
  *
- * This StepperMode module is derived from the existing StepperFirmata code,
- * but with the Sysex functionality removed, in order to more cleanly support
- * the FirmataCore/FirmataFeature/FirmataDevice architecture.
+ * This StepperMode class and its subclasses are derived from the existing
+ * StepperFirmata code, but with the Sysex functionality removed, in order
+ * to more cleanly support the FirmataCore/FirmataFeature/FirmataDevice
+ * architecture.
  *
  * Doug Johnson Dec 2015
- *
- * Formatted using the GNU C formatting and indenting
  */
 
 
 #include <FirmataCore.h>
 #include "StepperMode.h"
-// #include "utility/FirmataStepper.h"
-
-StepperModeClass::StepperModeClass() {
-
-}
 
 //---------------------------------------------------------------------------
 
@@ -43,39 +37,3 @@ void StepperModeClass::handleGetCapability(byte pin)
   }
 }
 
-void StepperModeClass::reset()
-{
-  for (byte i = 0; i < MAX_STEPPERS; i++) {
-    // if (stepper[i]) {
-    //   free(stepper[i]);
-    //   stepper[i] = 0;
-    // }
-  }
-  numSteppers = 0;
-}
-
-/*==============================================================================
- * LOOP()
- *============================================================================*/
-void StepperModeClass::update()
-{
-  // if (numSteppers > 0) {
-  //   // if one or more stepper motors are used, update their position
-  //   for (byte i = 0; i < MAX_STEPPERS; i++) {
-  //     if (stepper[i]) {
-  //       bool done = stepper[i]->update();
-  //       // send command to client application when stepping is complete
-  //       if (done) {
-  //         Firmata.write(START_SYSEX);
-  //         Firmata.write(STEPPER_DATA);
-  //         Firmata.write(i);
-  //         Firmata.write(END_SYSEX);
-  //       }
-  //     }
-  //   }
-  // }
-}
-
-// make one instance to coordinate enable/disable and IO across the app.
-
-StepperModeClass StepperMode;
--- libraries/FirmataCore/src/Mode/StepperMode.h
+++ libraries/FirmataCore/src/Mode/StepperMode.h
@@ -4,6 +4,15 @@
 #include <FirmataMode.h>
 
 #define MAX_STEPPERS 6 // arbitrary value... may need to adjust
+#
+/**
+ * This abstract class defines the methods that are available to a device
+ * driver that wishes to control stepper motors.  The concrete subclasses
+ * implement the methods as well as they can, depending on what underlying
+ * library they use.  Not all libraries can support all the defined methods
+ * fully, but the subclasses provide the best implementation they can, given
+ * the constraints of the library.
+ */
 
 class StepperModeClass: public FirmataMode {
 
@@ -13,19 +22,59 @@ public:
 
   // Firmata access to capabilities
 
-  boolean handleSetPinMode(byte pin, int mode);
-  void handleGetCapability(byte pin);
+  virtual boolean handleSetPinMode(byte pin, int mode);
+  virtual void handleGetCapability(byte pin);
 
   // Implementation of capabilities
 
-  void reset();
-  void update();
+  virtual void reset() = 0;
+  virtual void update() = 0;
+
+// Basic Firmata Stepper library
+
+  virtual void setSpeed(long whatSpeed) = 0;  // set desired constant speed
+  virtual void step(int number_of_steps) = 0; // blocking step control at constant speed
+
+// AccelStepper library
+
+  virtual void stop() = 0;
+
+  virtual boolean run() = 0;                  // async step control with accel/decel
+  virtual boolean runSpeed() = 0;             // async step control at constant speed
+  virtual boolean runSpeedToPosition() = 0;   // async step control at constant speed (calculates direction)
+
+  virtual void    runToPosition() = 0;                  // blocking step control with accel/decel
+  virtual void    runToNewPosition(long position) = 0;  // blocking step control with accel/decel
+
+  virtual void    moveTo(long absolute) = 0;  // set target position
+  virtual void    moveBy(long relative) = 0;  // set target position
+
+  virtual long    getCurrentPosition() = 0;
+  virtual long    getTargetPosition() = 0;
+  virtual long    getDistanceToGo() = 0;
+
+  virtual void    setSpeed(float speed) = 0;
+  virtual float   getSpeed() = 0;
+  virtual void    setMaxSpeed(float speed) = 0;
+  virtual float   getMaxSpeed() = 0;
+
+  virtual void    setAcceleration(float acceleration) = 0;
+  virtual void    setDeceleration(float deceleration) = 0;
+
+  virtual void    setCurrentPosition(long position) = 0;
+  virtual void    setMinPulseWidth(unsigned int minWidth) = 0;
+
+  virtual void    setPinsInverted(bool directionInvert = false, bool stepInvert = false, bool enableInvert = false) = 0;
+  virtual void    setPinsInverted(bool pin1Invert, bool pin2Invert, bool pin3Invert, bool pin4Invert, bool enableInvert) = 0;
+  virtual void    setEnablePin(uint8_t enablePin = 0xff) = 0;
+
+  virtual void    disableOutputs() = 0;
+  virtual void    enableOutputs() = 0;
+
 
 private:
   // FirmataStepper *stepper[MAX_STEPPERS];
   byte numSteppers;
 };
-
-extern StepperModeClass StepperMode;
 
 #endif
