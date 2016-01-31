b3cbc8a14b00f5e951211cb49eaabade003d5cd3
Doug Johnson <finson@whidbey.com>
2016-01-31 00:36:51 -0800

    Expand Hello testing to cover more functions and registers.

--- main/DriverTestHello/DriverTestHello.ino
+++ main/DriverTestHello/DriverTestHello.ino
@@ -1,122 +1,144 @@
 #include <FirmataDeviceLibrary.h>
-#include <Hello/HelloDriver.h>
-#include <Peek/PeekDriver.h>
+#include <Device/DeviceTable.h>
+#include <Framework/Logger.h>
+#include <Framework/Tester.h>
+#include <Framework/ByteOrder.h>
+#include "SelectedDeviceDrivers.h"
 
 /**
-   Test the operation of simple device driver HelloDriver.
+   Test the operation of the Hello World driver.
 */
-
-#define getInt16(lsb, msb) ((((msb) & 0xFF)<<8) | ((lsb) & 0xFF))
-#define getLSBInt16(val) ((val) & 0xFF)
-#define getMSBInt16(val) (((val) >> 8)& 0xFF)
-
-HelloDriver ddHello("HW");
-PeekDriver ddPeek("PK");
-
-DeviceDriver *ddArray[] {&ddHello, &ddPeek};
 
 #define BUF_SIZE 256
 byte buf[BUF_SIZE];
-char names[BUF_SIZE];
-int handle;
-int vReg[] = {static_cast<int>(CDR::DriverVersion), static_cast<int>(CDR::LibraryVersion)};
+
+DeviceTable *dt;
+Tester *tst;
+Logger *logger;
+
+// --------------------------------------------------------
 
 void setup() {
-  int bufIndex = 0;
+
+  // Countdown before running to give time to open the monitor window.
 
   Serial.begin(115200);
+
   delay(3000);
-  for (int idx = 5; idx > 0; idx--) {
-    Serial.println(idx);
+  for (int idx = 0; idx < 5; idx++) {
+    Serial.print(5 - idx);
+    Serial.print(" ");
     delay(1000);
   }
+  Serial.println();
 
-  for (int ddIndex = 0; ddIndex < 2; ddIndex++) {
-    DeviceDriver *dd = ddArray[ddIndex];
-    Serial.print("Open: ");
-    int status = dd->open("HW:0", 0);
-    Serial.println(status);
+  // Load the device driver table
 
-    //  Serial.print("Config: ");
-    //  handle = status;
-    //  int bufIndex = 0;
-    //  buf[bufIndex++] = 2;  // 4-wire interface
-    //  buf[bufIndex++] = getLSBInt16(StepsPerRevolution);
-    //  buf[bufIndex++] = getMSBInt16(StepsPerRevolution);
-    //  for (int idx = 0; idx < 4; idx++) {
-    //    buf[bufIndex++] = getLSBInt16(pin[idx]);
-    //    buf[bufIndex++] = getMSBInt16(pin[idx]);
-    //  }
-    //  status = dd->control(handle, CDR_Configure, bufIndex, buf);
-    //  Serial.println(status);
-    //
-    //  Serial.print("Speed: ");
-    //  bufIndex = 0;
-    //  buf[bufIndex++] = getLSBInt16(RevolutionsPerMinute);
-    //  buf[bufIndex++] = getMSBInt16(RevolutionsPerMinute);
-    //  buf[bufIndex++] = 0;
-    //  buf[bufIndex++] = 0;
-    //  status = dd->control(handle, STP_RPMSpeed, bufIndex, buf);
-    //  Serial.println(status);
+  dt = new DeviceTable(selectedDevices);
 
-    for (int regIndex = 0; regIndex < 2; regIndex++) {
-      Serial.print("Version: ");
-      status = dd->status(handle, vReg[regIndex], BUF_SIZE, buf);
-      Serial.print(status);
-      Serial.print(' ');
-      if (status >= 0) {
-        bufIndex = 0;
-        int packetSize = buf[bufIndex++];
-        for (int idx = 0; idx < packetSize; idx++) {
-          Serial.print(buf[bufIndex++]);
-          switch (idx) {
-            case 0:
-            case 1:
-              Serial.print('.');
-              break;
-            case 2:
-              Serial.print('-');
-              break;
-            case 3:
-            case 4:
-              Serial.print('.');
-              break;
-            case 5:
-              Serial.print(' ');
-              break;
-          }
-        }
-        Serial.write(reinterpret_cast<char *>(&buf[bufIndex]));
-      }
-      Serial.println();
-    }
+  //  Create the objects needed for the testing framework.
+
+  tst = new Tester();
+  logger = new Logger("DriverTestHello");
+  logger->setCurrentLogLevel(LogLevel::DEBUG);
+
+  // --------------------------------------------------------
+
+  // Begin testing
+
+  tst->beforeGroup("Hello");
+
+  // --------------------------------------------------------
+
+  tst->beforeTest("HelloOpenClose");
+  int status = dt->open("HW:0", DDO_FORCE_OPEN);
+  tst->assertTrue("Open error.", (status >= 0));
+  if (status >= 0) {
+    int handle = status;
+    status = dt->close(handle);
+    tst->assertTrue("Close error.", (status >= 0));
   }
+  tst->afterTest();
+
+  // --------------------------------------------------------
+
+  tst->beforeTest("HelloRead");
+  byte buf[256];
+  status = dt->open("HW:0", 0);
+  tst->assertTrue("Open error.", (status >= 0));
+
+  if (status >= 0) {
+    int handle = status;
+    status = dt->read(handle, 256, buf);
+    tst->assertTrue("Read error.", (status >= 0));
+
+    logger->debug("Read n bytes.", status);
+    status = dt->close(handle);
+    tst->assertTrue("Close error.", (status >= 0));
+  }
+  tst->afterTest();
+
+  // --------------------------------------------------------
+
+  tst->beforeTest("HelloCDRDriverVersion");
+  status = dt->open("HW:0");
+  tst->assertTrue("Open error.", (status >= 0));
+
+  int reg = static_cast<int>(CDR::DriverVersion);
+
+  if (status >= 0) {
+    int handle = status;
+    status = dt->status(handle, reg, BUF_SIZE, buf);
+    tst->assertTrue("Device status() Driver Version assertTrue:", (status >= 0));
+    if (status >= 0) {
+      logger->debug("Version data is n bytes. ", status);
+      //      char *svString = SemVer::toString(buf);
+      //      logger->debug("  Read n version bytes ", status,svString);
+      //      free(svString);
+    }
+    status = dt->close(handle);
+    tst->assertTrue("Close error.", (status >= 0));
+
+  }
+  tst->afterTest();
+
+  // --------------------------------------------------------
+
+  tst->beforeTest("HelloStatusMethod");
+  status = dt->open("HW:0");
+  tst->assertTrue("Open error.", (status >= 0));
+
+  if (status >= 0) {
+    int handle = status;
+
+    for (int r = 0; r < 2; r++) {
+      int count = BUF_SIZE;
+
+      status = dt->status(handle, r, count, buf);
+      tst->assertTrue("Device status() error.", (status >= 0));
+      tst->assertTrue("Device status() actual < max assertTrue:", (status < count));
+      if (status < 0) continue;
+
+      switch (r) {
+        
+        case static_cast<int>(HelloRegister::INTERJECTION):
+          logger->debug("Interjection byte count:", status);
+          break;
+          
+        case static_cast<int>(HelloRegister::OBJECT):
+          logger->debug("Object byte count:", status);
+          break;
+      }
+    }
+    status = dt->close(handle);
+    tst->assertTrue("Close error.", (status >= 0));
+  }
+  tst->afterTest();
+
+  // --------------------------------------------------------
+
+  tst->afterGroup();
+
 }
 
-void loop() {
-  //
-  //  Serial.print("Move+: ");
-  //  int msgIndex = 0;
-  //  buf[msgIndex++] = getLSBInt16(15);
-  //  buf[msgIndex++] = getMSBInt16(15);
-  //  buf[msgIndex++] = 0;
-  //  buf[msgIndex++] = 0;
-  //  buf[msgIndex++] = 0;
-  //  int status = dd->control(handle, STP_MoveR, msgIndex, buf);
-  //  Serial.println(status);
-  //
-  //  //delay(1000);
-  //
-  //  Serial.print("Move-: ");
-  //  msgIndex = 0;
-  //  buf[msgIndex++] = getLSBInt16(-15);
-  //  buf[msgIndex++] = getMSBInt16(-15);
-  //  buf[msgIndex++] = -1;
-  //  buf[msgIndex++] = -1;
-  //  buf[msgIndex++] = 0;
-  //  status = dd->control(handle, STP_MoveR, msgIndex, buf);
-  //  Serial.println(status);
-  //
-  //  //delay(1000);
-
-}
+void loop() {}
