8142d7eb8b06ac7b33185fd9a4ab917978d5d338
Doug Johnson <finson@whidbey.com>
2016-02-01 19:02:50 -0800

    Configure for stepper feature testing.

--- main/FirmataCoreMain/SelectedFeatures.h
+++ main/FirmataCoreMain/SelectedFeatures.h
@@ -16,24 +16,24 @@ AnalogOutputFeature analogOutput;
 #include <I2C/I2CFeature.h>
 I2CFeature ffI2C;
 
-#include <Device/DeviceFeature.h>
-DeviceFeature ffDevice;
+#include <Stepper/StepperFeature.h>
+StepperFeature ffStepper;
 
-// #include <Stepper/CommonStepper.h>
-//StepperFeature ffStepper;
+//#include <Device/DeviceFeature.h>
+//DeviceFeature ffDevice;
 
-FirmataFeature *selectedFeatures[] = {&digitalInput,&digitalOutput,&analogInput,&analogOutput,&ffI2C,&ffDevice,0};
-//FirmataFeature *selectedFeatures[] = {&digitalInput,&digitalOutput,&analogInput,&analogOutput,&ffI2C,&ffStepper,&ffDevice,0};
+FirmataFeature *selectedFeatures[] = {&digitalInput,&digitalOutput,&analogInput,&analogOutput,&ffStepper,&ffI2C,0};
+//FirmataFeature *selectedFeatures[] = {&digitalInput,&digitalOutput,&analogInput,&analogOutput,&ffStepper,&ffI2C,&ffDevice,0};
 
 // Device Drivers
 
- #include <Hello/HelloDriver.h>
- HelloDriver ddHello("Hello",2);
+// #include <Hello/HelloDriver.h>
+// HelloDriver ddHello("Hello",2);
 
 // #include <MCP9808/MCP9808Driver.h>
 // MCP9808Driver ddMCP9808("MCP9808",0x18,1);
 
 // DeviceDriver *selectedDevices[] = {0};
- DeviceDriver *selectedDevices[] = {&ddHello,0};
+// DeviceDriver *selectedDevices[] = {&ddHello,0};
 // DeviceDriver *selectedDevices[] = {&ddMCP9808,0};
 // DeviceDriver *selectedDevices[] = {&ddHello,&ddMCP9808,0};
