3fe600edc1f45e2a30fdb1330306ec5a84d910dd
Doug Johnson <finson@whidbey.com>
2016-02-10 23:05:13 -0800

    Prepare for 2.4 wrapup and release.
    Next release will be based on including DriverFirmata into
    ConfigurableFirmata.  This branch will be on hiatus rather than trying
    to revert it change by change.

--- main/FirmataCoreMain/SelectedFeatures.h
+++ main/FirmataCoreMain/SelectedFeatures.h
@@ -13,30 +13,30 @@ AnalogInputFirmata analogInput;
 #include <GPIO/AnalogOutputFirmata.h>
 AnalogOutputFirmata analogOutput;
 
-//#include <I2C/I2CFirmata.h>
-//I2CFirmata ffI2C;
+#include <I2C/I2CFirmata.h>
+I2CFirmata ffI2C;
 
 #include <Stepper/StepperFirmata.h>
 StepperFirmata ffStepper;
 
-#include <Servo/ServoFirmata.h>
-ServoFirmata ffServo;
+//#include <Servo/ServoFirmata.h>
+//ServoFirmata ffServo;
 
-//#include <Device/DeviceFirmata.h>
-//DeviceFirmata ffDevice("DeviceTable");
+#include <Device/DeviceFirmata.h>
+DeviceFirmata ffDevice;
 
-FirmataFeature *selectedFeatures[] = {&digitalInput,&digitalOutput,&analogInput,&analogOutput,&ffStepper,&ffServo,0};
-//FirmataFeature *selectedFeatures[] = {&digitalInput,&digitalOutput,&analogInput,&analogOutput,&ffStepper,&ffI2C,&ffDevice,0};
+FirmataFeature *selectedFeatures[] = {&digitalInput,&digitalOutput,&analogInput,&analogOutput,&ffI2C,&ffStepper,&ffDevice,0};
+//FirmataFeature *selectedFeatures[] = {&digitalInput,&digitalOutput,&analogInput,&analogOutput,&ffI2C,&ffStepper,&ffServo,&ffDevice,0};
 
 // Device Drivers
 
-// #include <Hello/HelloDriver.h>
-// HelloDriver ddHello("Hello",2);
-//
-// #include <MCP9808/MCP9808Driver.h>
-// MCP9808Driver ddMCP9808("MCP9808",0x18,1);
+ #include <Hello/HelloDriver.h>
+ HelloDriver ddHello("Hello",2);
+
+ #include <MCP9808/MCP9808Driver.h>
+ MCP9808Driver ddMCP9808("MCP9808",1,0x18);
 
 // DeviceDriver *selectedDevices[] = {0};
 // DeviceDriver *selectedDevices[] = {&ddHello,0};
 // DeviceDriver *selectedDevices[] = {&ddMCP9808,0};
-// DeviceDriver *selectedDevices[] = {&ddHello,&ddMCP9808,0};
+ DeviceDriver *selectedDevices[] = {&ddHello,&ddMCP9808,0};
